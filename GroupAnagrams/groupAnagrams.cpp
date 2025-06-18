2fb0d150eb329186f74fc4fd5cc0442d7b19aedd
Ayesha-Khan07 <89073826+Ayesha-Khan07@users.noreply.github.com>
2025-06-18 16:57:08 +0500

    Update groupAnagrams.cpp

--- GroupAnagrams/groupAnagrams.cpp
+++ GroupAnagrams/groupAnagrams.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+vector<vector <string>> groupAnagrams(vector<string> &strs)
+{
+  unordered_map <string, vector<string>> groupAna;
+  for(string word: strs)
+  {
+    string key = word;
+    sort(key.begin(), key.end());
+    groupAna[key].push_back(word);
+  }
+
+  vector<vector <string>> res;
+
+  // collect the vectr arrays from unordered map
+  for(auto & pair : groupAna)
+  {
+    res.push_back(pair.second);
+  }
+
+  return res;
+}
+    
+
+
 
